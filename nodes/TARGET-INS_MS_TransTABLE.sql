644ba3302ddf53b9f9d6c32800f89f4877238904
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-05-11 13:16:13 +0530

    Clear workspace

--- nodes/TARGET-INS_MS_TransTABLE.sql
+++ nodes/TARGET-INS_MS_TransTABLE.sql
@@ -1,7 +1,13 @@
-@id("4f1a8713-1b5a-4fc6-8d21-20aa2756089e")
+@id("3b4e9eb3-3bbd-4d2a-bf9a-1992a12736b2")
 @nodeType("6fda2820-4404-4b60-bad3-cf0edd7dab92")
 
 @materializationType("transient table")
+
+WITH ALL_TABLES AS (
+     SELECT * FROM {{ ref('SRC', 'NATION_COPY1') }} "NATION_COPY1"
+     UNION
+     SELECT * FROM {{ ref('SRC', 'NATION_COPY2') }} "NATION_COPY2"
+)
 
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY",
@@ -9,4 +15,4 @@ SELECT
      "N_REGIONKEY" AS "N_REGIONKEY",
      "N_COMMENT" AS "N_COMMENT",
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
-FROM {{ ref('SRC', 'NATION') }} "NATION"
+FROM ALL_TABLES
