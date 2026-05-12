c9582dd98e9737c403e10d3bf33d4901ce3f1ea8
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-05-12 15:47:32 +0530

    Restarting work with newer code

--- nodes/TARGET-INS_NATION_TEST.sql
+++ nodes/TARGET-INS_NATION_TEST.sql
@@ -1,10 +1,11 @@
-@id("4cd7b05e-69b0-4a32-85ed-1e22f846d2a1")
+@id("06e64389-9619-46b8-bdc5-39c7f759c404")
 @nodeType("6fda2820-4404-4b60-bad3-cf0edd7dab92")
+@selectDistinct(true)
+@testsEnabled(true)
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY",
      "N_NAME" AS "N_NAME",
      "N_REGIONKEY" AS "N_REGIONKEY",
      "N_COMMENT" AS "N_COMMENT",
-     UPPER("N_COMMENT") AS "N_COMMENT_UPPER",
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
 FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
