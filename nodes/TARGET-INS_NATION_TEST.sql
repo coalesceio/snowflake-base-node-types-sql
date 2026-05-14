d68257e9fcbc85ab99d2616eb1ed6313b8f71a24
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-05-14 12:19:28 +0530

    @hashValue("<hash_col_name>")

--- nodes/TARGET-INS_NATION_TEST.sql
+++ nodes/TARGET-INS_NATION_TEST.sql
@@ -2,9 +2,10 @@
 @nodeType("6fda2820-4404-4b60-bad3-cf0edd7dab92")
 
 SELECT
-     "N_NATIONKEY" AS "N_NATIONKEY" @nullable("false"),
-     "N_NAME" AS "N_NAME" @description("nation name") @defaultValue("NA"),
-     "N_REGIONKEY" AS "N_REGIONKEY" @nullable("false"),
+     "N_NATIONKEY" AS "N_NATIONKEY" @nullable("false") @hashValue("GH_COL"),
+     "N_NAME" AS "N_NAME" @description("nation name") @defaultValue("NA") @hashValue("GH_COL"),
+     "N_REGIONKEY" AS "N_REGIONKEY" @nullable("false") @hashValue("GH_COL"),
      "N_COMMENT" AS "N_COMMENT" @tests("null", "unique"),
-     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP",
+     {{ get_hash('GH_COL') }}::STRING AS "GH_COL"
 FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
