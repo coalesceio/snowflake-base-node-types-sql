6f7e84edef3206a5c01a232d71059fd4f86c89c1
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-05-14 11:08:51 +0530

    pre, post tests

--- nodes/TARGET-INS_NATION_TEST.sql
+++ nodes/TARGET-INS_NATION_TEST.sql
@@ -1,11 +1,9 @@
 @id("2182f347-3873-46d2-b042-e1da6841df4c")
 @nodeType("6fda2820-4404-4b60-bad3-cf0edd7dab92")
 
-@selectDistinct
+@preTests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "continuEOnFailure:SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1") 
 
-@preSQL("SELECT count(*) FROM {{ this }}", "SELECT count(*) FROM {{ this }}") 
-
-@postSQL("BEGIN SELECT count(*) FROM {{ this }}; SELECT count(*) FROM {{ this }}; END", "SELECT count(*) FROM {{ this }}", "SELECT count(*) FROM {{ this }}") 
+@postTests("continueOnFailure: SELECT * FROM {{ this }} WHERE N_NAME IS NULL", "SELECT * FROM {{ this }} WHERE N_REGIONKEY < 0") 
 
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY",
