d7f13804a304161d6c146c44235a354cf19db1e4
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-05-14 12:32:32 +0530

    Added with All column attributes

---
+++ nodes/TARGET-INS_NATION_TEST.sql
@@ -0,0 +1,9 @@
+@id("b1c6336d-3831-4833-84a7-11c7267a228c")
+@nodeType("6fda2820-4404-4b60-bad3-cf0edd7dab92")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY" @nullable("false") @defaultValue(100),
+     "N_NAME" AS "N_NAME" @description("Name"),
+     "N_REGIONKEY" AS "N_REGIONKEY" @nullable("false") @defaultValue(0),
+     "N_COMMENT" AS "N_COMMENT" @description("comment"),
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
