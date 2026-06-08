095d3a6017bc62a3c845904988e4c88146e52ef2
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-06-08 15:15:47 +0530

    Initial Deployment

---
+++ nodes/TARGET-WRK_NATION_TEST_V2.sql
@@ -0,0 +1,10 @@
+@id("4363ad8c-aa3e-46a7-9bfa-5192fb78fd67")
+@nodeType("664")
+SELECT
+     NATION_TEST."N_NATIONKEY" AS "N_NATIONKEY" @nullable("false") @hashValue("GH_COL"),
+     NATION_TEST."N_NAME" AS "N_NAME" @defaultValue("NA")  @hashValue("GH_COL"),
+     NATION_TEST."N_REGIONKEY" AS "N_REGIONKEY" @description("region key")  @hashValue("GH_COL"),
+     NATION_TEST."N_COMMENT" AS "N_COMMENT" @hashValue("GH_COL"),
+     NATION_TEST."N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @tests("null", "unique") @hashValue("GH_COL"),
+     {{ get_hash('GH_COL', 'SHA256', '~') }}::STRING AS "GH_COL"
+FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
