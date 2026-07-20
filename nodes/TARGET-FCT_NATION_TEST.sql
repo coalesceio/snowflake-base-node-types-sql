e7f69f81284338d98bf622303997015fa42a2e05
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-07-20 12:20:37 +0530

    get_source_transform -> get_source_transform_v2

--- nodes/TARGET-FCT_NATION_TEST.sql
+++ nodes/TARGET-FCT_NATION_TEST.sql
@@ -1,10 +1,14 @@
 @id("387b4361-9ebd-487a-8119-f9ca34d2d7d1")
 @nodeType("661")
+@testsEnabled(true)
+@tests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "Before", true)
+@tests("SELECT * FROM {{ this }} WHERE N_NAME IS NULL", "After", true)
+@tests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "After", true)
 SELECT
-     "N_NATIONKEY" AS "N_NATIONKEY",
-     "N_NAME" AS "N_NAME",
-     "N_REGIONKEY" AS "N_REGIONKEY",
-     "N_COMMENT" AS "N_COMMENT",
+     "N_NATIONKEY" AS "N_NATIONKEY" @tests('null'),
+     "N_NAME" AS "N_NAME" @tests("unique"),
+     "N_REGIONKEY" AS "N_REGIONKEY" ,
+     "N_COMMENT" AS "N_COMMENT" @tests("unique", 'null'),
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP",
      CAST(CURRENT_TIMESTAMP AS TIMESTAMP) AS "SYSTEM_CREATE_DATE",
      CAST(CURRENT_TIMESTAMP AS TIMESTAMP) AS "SYSTEM_UPDATE_DATE"
