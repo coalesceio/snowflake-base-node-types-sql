e76a2a2984ba58a144b219716c4bc4fc663abf14
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-07-29 11:54:06 +0530

    Macro fix

--- nodes/TARGET-WRK_NATION_T.sql
+++ nodes/TARGET-WRK_NATION_T.sql
@@ -1,12 +1,12 @@
 @id("c814974d-8fd5-48f2-9fb1-6a9aa9080a69")
 @nodeType("664")
 @orderby(true)
-@orderbycolumn("[object Object]", "desc")
 @groupByAll(true)
 @description("Table''desc")
 @testsEnabled(true)
 @tests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "After", true)
 @tests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "After", true)
+@orderbycolumn("[object Object]", "desc")
 WITH ALL_NATION AS
 (
 SELECT
