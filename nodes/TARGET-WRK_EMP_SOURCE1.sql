b15f5ba964717f2ef54ec6226bc2df4acb701238
Pranjal Mane <pranjal.mane.contractor@coalesce.io>
2026-07-29 12:20:28 +0530

    Work Node Deploy 2

---
+++ nodes/TARGET-WRK_EMP_SOURCE1.sql
@@ -0,0 +1,12 @@
+@id("46b1a105-6894-4e72-b9d9-14af05cc4c9a")
+@nodeType("664")
+@testsEnabled(true)
+@tests("SELECT 1 FROM {{ this }} GROUP BY EMP_NAME HAVING COUNT(*) > 1", "After", true)
+@tests("SELECT 1 FROM {{ this }} GROUP BY EMP_NAME HAVING COUNT(*) > 1", "After", true)
+SELECT
+     "EMP_ID" AS "EMP_ID",
+     "EMP_NAME" AS "EMP_NAME",
+     "EMP_CITY" AS "EMP_CITY",
+     "DML_FLAG" AS "DML_FLAG",
+     "CREATED_AT" AS "CREATED_AT"
+FROM {{ ref('SRC', 'EMP_SOURCE') }} "EMP_SOURCE"
