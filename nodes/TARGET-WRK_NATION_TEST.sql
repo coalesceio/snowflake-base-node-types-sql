bd100acd43049b7d23a579335710a6da9aaad9b8
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-07-31 12:22:44 +0530

    Clear space

--- nodes/TARGET-WRK_NATION_TEST.sql
+++
@@ -1,9 +0,0 @@
-@id("2940e54e-7357-4fee-845d-111a1d3aaf94")
-@nodeType("664")
-SELECT
-     "N_NATIONKEY" AS "N_NATIONKEY",
-     "N_NAME" AS "N_NAME",
-     "N_REGIONKEY" AS "N_REGIONKEY" @tests("null"),
-     "N_COMMENT" AS "N_COMMENT",
-     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @tests("unique")
-FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
