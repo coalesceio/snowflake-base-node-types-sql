374a070ed8f1bc2fc905a2d40ae1a01ba28e9feb
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-07-31 11:51:40 +0530

    deployment

--- nodes/TARGET-WRK_NATION_TEST.sql
+++ nodes/TARGET-WRK_NATION_TEST.sql
@@ -1,13 +1,5 @@
 @id("2940e54e-7357-4fee-845d-111a1d3aaf94")
 @nodeType("664")
-@selectDistinct(true)
-@orderby(true)
-@orderbycolumn("[object Object]", "desc")
-@orderbycolumn("[object Object]", "desc")
-@testsEnabled(true)
-@tests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "After", true)
-@tests("SELECT 1 FROM {{ this }} GROUP BY N_COMMENT HAVING COUNT(*) > 1", "Before", true)
-
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY",
      "N_NAME" AS "N_NAME",
