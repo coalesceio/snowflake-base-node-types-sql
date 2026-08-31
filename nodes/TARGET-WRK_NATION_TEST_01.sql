990c9c1b8cb3e6a30ee591c8f22038a43dab77d3
Pranjal Mane <pranjal.mane.contractor@coalesce.io>
2026-08-31 10:27:34 +0530

    cleanup

--- nodes/TARGET-WRK_NATION_TEST_01.sql
+++
@@ -1,5 +0,0 @@
-@id("be538795-ea15-43a0-9acf-61b15787dbd1")
-@nodeType("704")
-SELECT
-     "N_NAME" AS "N_NAME" @tests("null") @notNull @defaultValue("'NA'") @description("Name col")
-FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST" 
