44aed3e60dbdafaf0aeaee05181b358644dfc9f6
Pranjal Mane <pranjal.mane.contractor@coalesce.io>
2026-08-31 10:38:00 +0530

    Nation table - redeployment - Added default

--- nodes/TARGET-WRK_NATION_TEST_02.sql
+++ nodes/TARGET-WRK_NATION_TEST_02.sql
@@ -1,5 +1,5 @@
 @id("cdb05971-7d35-410d-872b-29a10bc37aed")
 @nodeType("704")
 SELECT
-     "N_NAME" AS "N_NAME"
+     "N_NAME" AS "N_NAME" @defaultValue("NA")
 FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
