aff1826f7ccfd888de4d27cf948b5bd25f197e71
prj2001-udn <169126315+prj2001-udn@users.noreply.github.com>
2026-09-03 12:27:16 +0530

    Sales,unpivot,pivot,nation-V2-added

---
+++ nodes/TARGET-WRK_NATION_TEST_freshness.sql
@@ -0,0 +1,16 @@
+@id("4dbeedf4-1d30-4142-bf65-3f9ddf1d63f7")
+@nodeType("707")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY",
+     "N_NAME" AS "N_NAME",
+     "N_REGIONKEY" AS "N_REGIONKEY",
+     "N_COMMENT" AS "N_COMMENT",
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP",
+     "N_LOAD_TIMESTAMP" AS "FR_SECOND" @freshness(1, "SECOND"),
+     "N_LOAD_TIMESTAMP" AS "FR_MINUTE" @freshness(1, "MINUTE"),
+     "N_LOAD_TIMESTAMP" AS "FR_HOUR" @freshness(1, "HOUR"),
+     "N_LOAD_TIMESTAMP" AS "FR_DAY" @freshness(1, "DAY"),
+     "N_LOAD_TIMESTAMP" AS "FR_WEEK" @freshness(1, "WEEK"),
+     "N_LOAD_TIMESTAMP" AS "FR_MONTH" @freshness(1, "MONTH"),
+     "N_LOAD_TIMESTAMP" AS "FR_YEAR" @freshness(1, "YEAR")
+FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
---
+++ nodes/TARGET-WRK_RAW_SALES_QUALIFY.sql
@@ -0,0 +1,9 @@
+@id("d5701e06-33d2-49f9-9baa-88b497cc6527")
+@nodeType("707")
+SELECT 
+    Salesperson, 
+    Sale_Date, 
+    SUM(Amount) AS Total_Daily_Sales
+FROM {{ ref('TARGET', 'RAW_SALES') }} "RAW_SALES"
+GROUP BY Salesperson, Sale_Date
+QUALIFY ROW_NUMBER() OVER (PARTITION BY Salesperson ORDER BY SUM(Amount) DESC) = 1;
