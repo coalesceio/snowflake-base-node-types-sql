b3acc00e37d778d6603f6118866e94eb26a5b5c2
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-03 16:38:07 +0530

    COMMIT-20260903-materializationtype-incorrect-node-added2

--- nodes/SRC-WRK_PRODUCTS_GROCERY.sql
+++ nodes/SRC-WRK_PRODUCTS_GROCERY.sql
@@ -1,6 +1,6 @@
 @id("16b0c3a4-9750-44a6-82ae-32bca7a28365")
 @nodeType("707")
-@materializationType("Table")
+@materializationType("TableView")
 SELECT
      "PRODUCT_ID" AS "PRODUCT_ID",
      "PRODUCT_NAME" AS "PRODUCT_NAME",
