c6b58132195dec8b525c87e040b59f1df4f135d0
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-03 11:47:45 +0530

    COMMIT-20260903_DELETES

--- nodes/TARGET-WRK_CUSTOMER2.sql
+++
@@ -1,9 +0,0 @@
-@id("0ae7382c-ee31-4c39-8a35-d5c3bc29fa28")
-@nodeType("707")
-SELECT
-     "C_CUSTKEY" AS "C_CUSTKEY",
-     "C_NAME" AS "C_NAME",
-     "C_ADDRESS" AS "C_ADDRESS",
-     "C_NATIONKEY" AS "C_NATIONKEY",
-     "C_PHONE" AS "C_PHONE"
-FROM {{ ref('SRC', 'CUSTOMER') }} "CUSTOMER"
