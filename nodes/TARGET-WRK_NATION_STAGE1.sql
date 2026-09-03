2c4059936d97c10714a277b05db7eddded26817b
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-03 11:10:14 +0530

    Added pipeline1, job, environment

--- nodes/TARGET-WRK_NATION_STAGE1.sql
+++ nodes/TARGET-WRK_NATION_STAGE1.sql
@@ -1,7 +1,10 @@
 @id("3fc0b050-3eb4-420f-9d1a-561ab12a10ed")
 @nodeType("707")
+@description("V1 Stage -> V2 Work -> V1 Stage pipeline example")
+@writeMode("truncateInsert")
+@tests("SELECT 1 FROM {{ this }} GROUP BY N_NATIONKEY HAVING COUNT(*) > 1", true, "After")
 SELECT
-     "N_NATIONKEY" AS "N_NATIONKEY",
-     "N_NAME" AS "N_NAME",
-     "N_REGIONKEY" AS "N_REGIONKEY"
+     "N_NATIONKEY" AS "N_NATIONKEY" @not_null @uniqueness,
+     "N_NAME" AS "N_NAME" @not_null,
+     "N_REGIONKEY" AS "N_REGIONKEY" @min_max("0", "4")
 FROM {{ ref('TARGET', 'STG_NATION_STAGE1') }} "STG_NATION_STAGE1"
