66e847c5878cd9920faaa1ac882c27ddc8982cda
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-03 14:00:27 +0530

    deployment

---
+++ nodes/SRC-WRK_NATION_TEST_VIEW.sql
@@ -0,0 +1,10 @@
+@id("2a77fc94-f426-4db8-ae12-db44553ba07a")
+@nodeType("707")
+@materializationType("view")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY" @not_null,
+     "N_NAME" AS "N_NAME",
+     "N_REGIONKEY" AS "N_REGIONKEY",
+     "N_COMMENT" AS "N_COMMENT",
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+FROM {{ ref('SRC', 'STG_NATION_TEST_VIEW') }} "STG_NATION_TEST_VIEW"
