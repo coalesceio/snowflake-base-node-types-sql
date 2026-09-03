a975c0ad2a8643e018e736ce3f5d377022f5ce7e
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-03 11:43:38 +0530

    COMMIT-20260903_1143

--- nodes/TARGET-WRK_EMPLOYEES.sql
+++
@@ -1,8 +0,0 @@
-@id("9a4c5413-2ea8-4c30-a918-233a663b15a1")
-@nodeType("704")
-SELECT
-     e.EMPLOYEE_ID AS "emp_id",
-     m.EMPLOYEE_ID  AS "man_id"
-FROM {{ ref('SRC', 'EMPLOYEES') }} e
-LEFT JOIN {{ ref('SRC', 'EMPLOYEES') }}  m 
-    ON e.Manager_ID = m.Employee_ID;
