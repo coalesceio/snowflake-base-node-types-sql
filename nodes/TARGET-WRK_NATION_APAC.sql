59d6fdb25c2e6f190d8dd8deede613cd8a23ea67
DE-Tanvi <tanvi.kamat@dataentrega.com>
2026-09-03 12:23:29 +0530

    Add V2 Work -> V1 Stage pipeline example, declare platformKind

---
+++ nodes/TARGET-WRK_NATION_APAC.sql
@@ -0,0 +1,8 @@
+@id("bef59018-6a23-4505-87b5-070ca420ff81")
+@nodeType("707")
+@description("V2 Work node feeding a downstream V1 Stage node")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY" @not_null @uniqueness,
+     "N_NAME" AS "N_NAME" @not_null,
+     "N_REGIONKEY" AS "N_REGIONKEY" @min_max("0", "4")
+FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
