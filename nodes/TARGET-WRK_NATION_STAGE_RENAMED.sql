45db74de0bad8f3936ef8d1a6c9b4713df88b991
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-03 11:21:48 +0530

    P1: annotations chnage

--- nodes/TARGET-WRK_NATION_STAGE_RENAMED.sql
+++ nodes/TARGET-WRK_NATION_STAGE_RENAMED.sql
@@ -5,6 +5,6 @@
 @tests("SELECT 1 FROM {{ this }} GROUP BY N_NATIONKEY HAVING COUNT(*) > 1", true, "After")
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @not_null @uniqueness,
-     "N_NAME" AS "N_NAME",
-     "N_REGIONKEY" AS "N_REGIONKEY" @min_max("0", "3") @not_null
+     "N_NAME" AS "N_NAME" @not_null,
+     "N_REGIONKEY" AS "N_REGIONKEY" @min_max("0", "8") 
 FROM {{ ref('TARGET', 'STG_NATION_STAGE_RENAME') }} "STG_NATION_STAGE_RENAME"
