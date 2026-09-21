cd2d888bb6e99d95381e0a31e9b345ad0a7cde2c
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-21 15:34:12 +0530

    @disableIDs  in WITHID

--- nodes/TARGET-DIM_NATION_TEST_WITHID.sql
+++ nodes/TARGET-DIM_NATION_TEST_WITHID.sql
@@ -1,11 +1,12 @@
 @id("9f68afb8-4848-4dce-8508-3e2cb7b1e7e4")
 @nodeType("718")
+@disableIDs
 SELECT
     0                                        AS "DIM_NATION_TEST1_KEY" @id("5df2fd") @isSurrogateKey,
     "N_NATIONKEY"                            AS "N_NATIONKEY"          @id("e2f691") @isBusinessKey,
     "N_NAME"                                 AS "N_NAME"               @id("3c818f"),
     "N_REGIONKEY"                            AS "N_REGIONKEY_RENAME"          @id("d916b6"),
-    "N_COMMENT"                              AS "N_COMMENT"            @id("802956"),
+    "N_COMMENT"                              AS "N_COMMENT_RENAME"            @id("802956"),
     "N_LOAD_TIMESTAMP"                       AS "N_LOAD_TIMESTAMP"     @id("317113"),
     1                                        AS "SYSTEM_VERSION"       @id("5e6791") @isSystemVersion,
     'Y'                                      AS "SYSTEM_CURRENT_FLAG"  @id("3c0863") @isSystemCurrentFlag,
