7f64d5b16af4dbb3b45f5bf467e7f4ed5c7de330
DE-Tanvi <tanvi.kamat@dataentrega.com>
2026-09-22 12:38:16 +0530

    Enable disableIDs and remove now-unneeded @disableIDs template block; regenerate DIM_NATION_TEST

--- nodes/TARGET-DIM_NATION_TEST.sql
+++ nodes/TARGET-DIM_NATION_TEST.sql
@@ -1,12 +1,9 @@
-@id("8c51031b-383e-427b-b5af-386035acd330")
+@id("49706c21-3694-45d1-ae71-6a145d5c9d85")
 @nodeType("718")
-@mergeStrategy("upsert")
 SELECT
-    N_NATIONKEY AS "N_NATIONKEY" @id("7f9534") @isBusinessKey,
-    N_NAME AS "N_NAME" @id("ebd6b9"),
-    N_REGIONKEY AS "N_REGIONKEY" @id("b62c1a"),
-    N_COMMENT AS "N_COMMENT" @id("07c481"),
-    N_LOAD_TIMESTAMP AS "N_LOAD_TIMESTAMP" @id("116378") @lastModifiedTracking(1),
-    CAST(CURRENT_TIMESTAMP AS TIMESTAMP) AS "SYSTEM_CREATE_DATE" @id("599487") @isSystemCreateDate,
-    CAST(CURRENT_TIMESTAMP AS TIMESTAMP) AS "SYSTEM_UPDATE_DATE" @id("0b500c") @isSystemUpdateDate
-FROM {{ ref("SRC", "NATION_TEST") }}
+     "N_NATIONKEY" AS "N_NATIONKEY",
+     "N_NAME" AS "N_NAME",
+     "N_REGIONKEY" AS "N_REGIONKEY",
+     "N_COMMENT" AS "N_COMMENT",
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
