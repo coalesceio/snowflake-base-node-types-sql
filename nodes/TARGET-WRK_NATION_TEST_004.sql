e649ee874c1c50dfd29450f00760dfd25411f074
Pranjal Mane <pranjal.mane.contractor@coalesce.io>
2026-09-25 10:56:24 +0530

    Updated disableTests, continueOnFailure, min_max, min, max, accpeted and rejected values annotations

---
+++ nodes/TARGET-WRK_NATION_TEST_004.sql
@@ -0,0 +1,10 @@
+@id("3c931d5e-1fb8-4bda-bb58-df4d0b7695dc")
+@nodeType("707")
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY" @min_max("-5", "10") @accepted_values("1","2","3"),
+     "N_NAME" AS "N_NAME",
+     "N_REGIONKEY" AS "N_REGIONKEY" @min_value("-100") @accepted_values(1),
+     "N_COMMENT" AS "N_COMMENT",
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP" @max_value("'2024-01-01 00:00:00'"),
+     "N_DATE" AS "N_DATE" 
+FROM {{ ref('SRC', 'NATION_TEST') }} "NATION_TEST"
